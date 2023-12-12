4ad772b592b3937839d3d6948ab76ab56dae4563
Ananth <ananth@white.inc>
2023-12-12 10:45:43 +0530

    faq page done

--- assets/application.js
+++ assets/application.js
@@ -1,2 +1,10 @@
 // Put your application javascript here
 // faq page
+
+const questionHeaders = document.querySelectorAll('.faq-page-question-header')
+
+questionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        header.parentElement.classList.toggle("active");
+    })
+}) 
